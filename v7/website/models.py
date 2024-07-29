42dfa7ad3ef4f98a3eee7d4034fb34d38d6c4031
spicyjuicebox <iamshoock@gmail.com>
2024-07-30 09:24:33 +1200

    Made New Version Control Folder (v7).
    This version control folder was made for implementing the price being updated in the cart.

---
+++ v7/website/models.py
@@ -0,0 +1,51 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    orders = db.relationship('Order')
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    items = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_email = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    payment_method = db.Column(db.String(50), nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
---
+++ v7/website/views.py
@@ -0,0 +1,174 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
+from flask_login import login_required, current_user
+from .models import Post, User, Like, Comment, Order
+from . import db
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/")
+@views.route("/home")
+def home():
+    return render_template("home.html", user=current_user)
+
+
+@views.route("/blog")
+@login_required
+def blog():
+    posts = Post.query.all()
+    return render_template("blog.html", user=current_user, posts=posts)
+
+# New
+@views.route("/order", methods=['GET', 'POST'])
+@login_required
+def menu():
+    if request.method == 'POST':
+        order = request.form.get('order')
+        total = request.form.get('total')
+        if order and total:
+            return redirect(url_for('views.payment_option', order=order, total=total))
+    return render_template("order.html", user=current_user)
+
+@views.route('/payment_option', methods=['GET', 'POST'])
+@login_required
+def payment_option():
+    order = request.args.get('order')
+    total = request.args.get('total')
+    if not order or not total:
+        flash('No order found.', category='error')
+        return redirect(url_for('views.menu'))
+    return render_template('payment_option.html', user=current_user, order=order, total=total)
+
+@views.route('/cash_payment', methods=['GET', 'POST'])
+@login_required
+def cash_payment():
+    order = request.args.get('order')
+    total = request.args.get('total')
+    if not order or not total:
+        flash('No order found.', category='error')
+        return redirect(url_for('views.menu'))
+    new_order = Order(items=order, user_id=current_user.id, user_email=current_user.email, payment_method='Cash', total_price=total)
+    db.session.add(new_order)
+    db.session.commit()
+    return render_template('cash_payment.html', user=current_user, order=order, total=total)
+
+@views.route('/card_payment', methods=['GET', 'POST'])
+@login_required
+def card_payment():
+    order = request.args.get('order')
+    total = request.args.get('total')
+    if not order or not total:
+        flash('No order found.', category='error')
+        return redirect(url_for('views.menu'))
+    return render_template('card_payment.html', user=current_user, order=order, total=total)
+
+@views.route('/process_card_payment', methods=['POST'])
+@login_required
+def process_card_payment():
+    order = request.form.get('order')
+    total = request.form.get('total')
+    new_order = Order(items=order, user_id=current_user.id, user_email=current_user.email, payment_method='Card', total_price=total)
+    db.session.add(new_order)
+    db.session.commit()
+    return redirect(url_for('views.thank_you'))
+
+@views.route('/thankyou', methods=['GET'])
+@login_required
+def thank_you():
+    return render_template('thankyou.html', user=current_user)
+# End
+
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        text = request.form.get('text')
+        if not text:
+            flash('Post cannot be empty.', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category='success')
+            return redirect(url_for('views.blog'))
+    return render_template('create_post.html', user=current_user)
+
+@views.route("/like-post/<post_id>", methods=['POST'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+    if not post:
+        return jsonify({'error': 'Post does not exist.'}, 400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
+
+
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+    
+    posts = Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif post.author != current_user.id:
+        flash('You do not have permission to delete this post.', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted.', category='success')
+    return redirect(url_for('views.blog'))
+
+
+
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+    return redirect(url_for('views.blog'))
+
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+    return redirect(url_for('views.blog'))
