027001d79ea6d22431e67770498ca58ac0714c07
spicyjuicebox <iamshoock@gmail.com>
2024-08-01 16:05:15 +1200

    Updated the style.css and app.js files.
    Fixing the background colours of the navigation bar.

--- v7/website/static/app.js
+++ v7/website/static/app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const header2 = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -12,8 +13,10 @@ document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
 		header.style.backgroundColor = 'rgb(28, 74, 154)';
+		header2.style.backgroundColor = 'rgb(28, 74, 154)';
 	} else {
 		header.style.backgroundColor = 'transparent';
+		header2.style.backgroundColor = 'rgb(28, 74, 154)';
 	}
 });
 
