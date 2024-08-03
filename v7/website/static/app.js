241a310e1deca444eddb4effae0d921267b2e9e4
spicyjuicebox <iamshoock@gmail.com>
2024-08-03 12:05:35 +1200

    Updated the style.css and app.js files (2).
    Made more changes to the style.css, app.js and now template2.html files because there were still a few issues with the new header, header2, properly working.

--- v7/website/static/app.js
+++ v7/website/static/app.js
@@ -2,7 +2,11 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
-const header2 = document.querySelector('.header.container');
+
+const hamburger2 = document.querySelector('.header2 .nav-bar .nav-list .hamburger');
+const mobile_menu2 = document.querySelector('.header2 .nav-bar .nav-list ul');
+const menu_item2 = document.querySelectorAll('.header2 .nav-bar .nav-list ul li a');
+const header2 = document.querySelector('.header2.container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -12,10 +16,10 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = 'rgb(28, 74, 154)';
-		header2.style.backgroundColor = 'rgb(28, 74, 154)';
+		header.style.backgroundColor = 'rgb(28, 74, 154)'; /* When scrolling down, navigation bar changes to this colour. */
+		header2.style.backgroundColor = 'rgb(255, 0, 0)';
 	} else {
-		header.style.backgroundColor = 'transparent';
+		header.style.backgroundColor = 'transparent'; /* When scrolling, this will colour the background of the navigation bar. When going to the top of the page, the background will be transparent. */
 		header2.style.backgroundColor = 'rgb(28, 74, 154)';
 	}
 });
