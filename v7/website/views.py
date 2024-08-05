fc468b36c9532b4b50e6cc5a863e7327c34c51ea
spicyjuicebox <iamshoock@gmail.com>
2024-08-05 15:09:02 +1200

    Updating the card payment page.
    Changing the way that the text looks and the boxes for input.

--- v7/website/views.py
+++ v7/website/views.py
@@ -50,7 +50,7 @@ def cash_payment():
     new_order = Order(items=order, user_id=current_user.id, user_email=current_user.email, payment_method='Cash', total_price=total)
     db.session.add(new_order)
     db.session.commit()
-    return render_template('cash_payment.html', user=current_user, order=order, total=total, total_price=total)
+    return render_template('cash_payment.html', user=current_user, order=order, total=total, total_price=total) # The total_price brings the total price from the cart.
 
 @views.route('/card_payment', methods=['GET', 'POST'])
 @login_required
@@ -63,7 +63,7 @@ def card_payment():
     new_order = Order(items=order, user_id=current_user.id, user_email=current_user.email, payment_method='Card', total_price=total)
     db.session.add(new_order)
     db.session.commit()
-    return render_template('card_payment.html', user=current_user, order=order, total=total)
+    return render_template('card_payment.html', user=current_user, order=order, total=total, total_price=total) # The total_price brings the total price from the cart.
 
 @views.route('/process_card_payment', methods=['POST'])
 @login_required
