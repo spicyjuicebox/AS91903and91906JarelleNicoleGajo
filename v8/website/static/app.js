5c2d56dabdd0c24ff33194f779a5715dfdab909a
spicyjuicebox <iamshoock@gmail.com>
2024-08-06 21:09:45 +1200

    Updated the app.js file.
    Noticed that the menu_item was missing for the second version of the header in template2.html.

--- v8/website/static/app.js
+++ v8/website/static/app.js
@@ -1,18 +1,28 @@
+/* For First Version of Header */
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+/* For Second Version of Header */
 const hamburger2 = document.querySelector('.header2 .nav-bar .nav-list .hamburger');
 const mobile_menu2 = document.querySelector('.header2 .nav-bar .nav-list ul');
 const menu_item2 = document.querySelectorAll('.header2 .nav-bar .nav-list ul li a');
 const header2 = document.querySelector('.header2.container');
 
+/* For First Version of Header */
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
+/* For Second Version of Header */
+hamburger2.addEventListener('click', () => {
+	hamburger2.classList.toggle('active');
+	mobile_menu2.classList.toggle('active');
+});
+
+/* For First and Second Version of Header */
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
@@ -24,9 +34,18 @@ document.addEventListener('scroll', () => {
 	}
 });
 
+/* For First Version of Header */
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
+	});
+});
+
+/* For Second Version of Header */
+menu_item2.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger2.classList.toggle('active');
+		mobile_menu2.classList.toggle('active');
 	});
 });
