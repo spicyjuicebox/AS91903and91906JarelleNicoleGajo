d49d8461dceaea689b04be08587dc63a6fe635e4
spicyjuicebox <iamshoock@gmail.com>
2024-08-07 10:44:52 +1200

    Paginating.
    Doing the paginating to set the amount of posts that appear on each page and having buttons at the bottom of the page to show more posts.

--- v8/website/views.py
+++ v8/website/views.py
@@ -15,7 +15,9 @@ def home():
 @views.route("/blog")
 @login_required
 def blog():
-    posts = Post.query.all()
+    # New for paginating.
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_created.desc()).paginate(page=page, per_page=4)
     return render_template("blog.html", user=current_user, posts=posts)
 
 # New
